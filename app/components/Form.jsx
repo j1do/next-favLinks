62a078146ad1b04d28acba758565c2d9ea586be8
Cam Flowers <cam@floreolabs.org>
2025-03-05 21:39:39 -0500

    passing state using props

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -1,7 +1,41 @@
-function Form(){
+import {useState} from 'react'
+
+function Form(props){
+
+    // state - a way for us to set and reference data that changes in our component
+    const [name, setName] = useState("")
+    const [URL, setURL] = useState("")
     
     function handleClick(){
-        alert("Clicked!")
+       // reference the data inside of the input
+
+        //    console.log(name, URL)
+
+        // alert the parent component to pass some data as props to the table
+        // take the last value of the input and send it to the table
+
+        // let favLink = {
+        //     name: name,
+        //     URL: URL
+        // }
+
+        props.submitFavLink({name, URL})
+
+    }
+
+
+    function handleNameInputChange(event){
+        // we can use the event object to look at our input
+        console.log(event.target.value)
+        // save this data for later
+        setName(event.target.value) // update the state with our data 
+    }
+
+    function handleURLInputChange(event){
+        // we can use the event object to look at our input
+        console.log(event.target.value)
+        // save this data for later
+        setURL(event.target.value) // update the state with our data
     }
 
     
@@ -10,10 +44,10 @@ function Form(){
             {/* Form for the user to input data */}
             <form>
                 <label> Name </label>
-                <input type="text" />
+                <input type="text" onChange={handleNameInputChange}/>
 
                 <label> URL </label>
-                <input type="text" />
+                <input type="text" onChange={handleURLInputChange}/>
             </form>
 
             <button onClick={handleClick}> Submit </button>
--- app/page.jsx
+++ app/page.jsx
@@ -10,16 +10,31 @@ import { useState } from "react" // we use this to add state to our components
 import Form from "./components/Form"
 import Table from "./components/Table"
 
-function HomePage(){    
+function HomePage(){   
+
+    const [newFavLink, setNewFavLink] = useState({})
+    
+    function handleNewFavLink(favLink){
+        // favlink is an object containing a {name, URL}
+
+        console.log(favLink, "in HomePage")
+
+        setNewFavLink(favLink)
+    }
+    
+
     return (
         <div>
             <h1> FavLinks </h1>
+       
+       {/* The Form is responsible for gathering the data
+        and alerting the HomePage when it needs to pass it to the table  */}
+           
+            <Form submitFavLink={handleNewFavLink} /> 
 
-            <Form />
-
-            {/* A table the user can use to see their submissions */}
             
-            <Table />
+            
+            <Table data={newFavLink}/>
             
 
 
